b73da16281da7cd7bc8de0cbfa28248a6b3b4919
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2020-07-17 00:28:50 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,20 +14,6 @@ class CashRegister
     def add_item(title, price, quantity = 1)
         @total += price * quantity
         @shopping_cart << {:item => title, :price => price, :quantity => quantity}
-
-        # if !@shopping_cart[title]
-        #     @shopping_cart[title] = []
-        # end
-        # @shopping_cart[title] << price.to_i 
-        
-        # if quantity != nil 
-        #     @shopping_cart[title][price] << quantity
-        # else 
-        #     quantity = 1 
-        #     @shopping_cart[title][price] << quantity
-        # end
-        # @total += @shopping_cart[title][price]
-
     end
     
     def apply_discount
@@ -40,20 +26,12 @@ class CashRegister
     end
 
     def items
-        # {:item=>"eggs", :price=>1.99, :quantity=>1}, {:item=>"tomato", :price=>1.76, :quantity=>3}]
-        # [ eggs, tomato, tomato, tomato]
-        # array = [1,2,3,4,5]
-        # array.fill(12, array.size, 4)
-        
-        
         item_list = []
-        #item_list << @shopping_cart.each {|hash| hash[:item]}
-        #@shopping_cart.each {|hash| item_list << hash[:item].}
         item_list = @shopping_cart.collect do |hash| 
             [].fill(hash[:item], 0, hash[:quantity])
         end
         item_list.flatten 
-        # binding.pry
+        
     end
 
     def void_last_transaction
